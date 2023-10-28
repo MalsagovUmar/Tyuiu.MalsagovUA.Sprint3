8c9a029e2e4d8d9571b8aa1a525478a644c15973
Tyuiu.MalsagovUA@outlook.com <mals.umar@yandex.ru>
2023-10-28 19:16:38 +0500

    Task 4

---
+++ Tyuiu.MalsagovUA.Sprint3.Task4.V29.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MalsagovUA.Sprint3.Task4.V29.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MalsagovUA.Sprint3.Task4.V29.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e62e5b30-dc69-416b-9db2-94684d998de8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.MalsagovUA.Sprint3.Task4.V29/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MalsagovUA.Sprint3.Task4.V29.Lib;
+namespace Tyuiu.MalsagovUA.Sprint3.Task4.V29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            Console.Title = "Спринт #3 | Выполнил: Мальсагов У.А. | АСОиУБ-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Мальсагов Умар Асланович | АСОиУБ-23-2                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+            Console.WriteLine("* функции y = cos(x) / x. При х = 0 прервать цикл.                        *");
+            Console.WriteLine("* Полученные значения суммировать.                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Начало интервала: {startValue}");
+            Console.WriteLine($"Конец интервала: {stopValue}");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Сумма ряда = " + ds.Calculate(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
